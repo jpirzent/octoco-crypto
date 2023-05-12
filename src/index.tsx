3fc8deec3b88c7a554cb1f862c18e1efce1607e5
James Pirzenthal <james@eldoenergy.com>
2023-05-12 12:31:00 +0200

    feat: implement materialUI theme file

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#BF55EC",
+    },
+    secondary: {
+      main: "#EEEEEE",
+    },
+    background: {
+      default: "#FCF9F0",
+    },
+  },
+});
+
+export default theme;
